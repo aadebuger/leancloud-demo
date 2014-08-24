188b77289bb2f197442fd8b04eccec19019a2702
lzwjava <lzwjava@gmail.com>
2014-08-25 01:03:08 +0800

    use holo light and push demo tweaks

--- android/AVOSCloud-Push/src/com/avos/avoscloud/PushDemo/PushDemo.java
+++ android/AVOSCloud-Push/src/com/avos/avoscloud/PushDemo/PushDemo.java
@@ -8,9 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.alibaba.fastjson.JSONObject;
 import com.avos.avoscloud.*;
+
 
 public class PushDemo extends Activity {
   /**
